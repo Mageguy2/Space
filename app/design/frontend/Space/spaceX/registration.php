77f5047ef8e6f7e30628ba77a6df2e2ae8cbd38c
Mageguy2 <[mageguy2@gmail.com]>
2020-09-20 13:46:34 +0530

    Initial commit

---
+++ app/design/frontend/Space/spaceX/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © Space Themes. All rights reserved.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Space/spaceX',
+    __DIR__
+);
